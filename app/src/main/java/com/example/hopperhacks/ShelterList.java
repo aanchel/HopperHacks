e4c6147d076b71f2235d82c033501382c96ade0f
aanchel <angelt2070@gmail.com>
2019-03-23 14:40:37 -0700

    added back end and shelter list bttn and list page

--- app/src/main/java/com/example/hopperhacks/ShelterList.java
+++ app/src/main/java/com/example/hopperhacks/ShelterList.java
@@ -12,8 +12,37 @@ public class ShelterList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_list);
-        //textViewList = (TextView) findViewById(R.id.textViewList);
+        textViewList = (TextView) findViewById(R.id.textViewList);
 
         ShelterLogin.printList();
+
+        ArrayList<Shelter> unsorted = ShelterLogin.unsorted;
+
+        ArrayList<Shelter> homeless = ShelterLogin.homeless;
+        ArrayList<Shelter> hurricane = ShelterLogin.hurricane;
+        ArrayList<Shelter> pet = ShelterLogin.pet;
+        ArrayList<Shelter> earthquake = ShelterLogin.earthquake;
+        String temp = "";
+        for (int i = 0; i < homeless.size(); i++) {
+            temp += homeless.get(i).toString() + "\n";
+        }
+        String temp2 = "";
+        for (int i = 0; i < hurricane.size(); i++) {
+            temp2 += hurricane.get(i).toString() + "\n";
+        }
+        String temp3 = "";
+        for (int i = 0; i < pet.size(); i++) {
+            temp3 += pet.get(i).toString() + "\n";
+        }
+        String temp4 = "";
+        for (int i = 0; i < earthquake.size(); i++) {
+            temp4 += earthquake.get(i).toString() + "\n";
+        }
+        String print1 = "Homeless Shelters near you: \n\r";
+        String print2 = "Hurricane Shelters near you: \n\r";
+        String print3 = "Pet Shelters near you: \n\r";
+        String print4 = "Earthquake Shelters near you: \n\r";
+
+        textViewList.setText(print1 + temp + print2 + temp2 + print3 + temp3+ print4+ temp4);
     }
 }
